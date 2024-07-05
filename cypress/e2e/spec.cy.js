74e950537ed80a2009864977b7a6315db8c2e949
Aram-Khach <xaram3306@gmail.com>
2024-07-06 02:19:25 +0700

    first commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,17 @@
+describe('test login', function(){
+  it('register', function(){
+    cy.visit('https://unsplash.com/')
+    function logout (){
+      cy.get("input type"=[LaaWe Qzy6N ogSND ZR5jm cs1e4 xMiih ZR5jm GcCli v2BPu mP89P szXtT]").click();
+     // cy.get("[data-cy=sign-out-button]").click();
+    }
+    
+    //.get('input[type="email"]').type('kopa123@gmail.com')
+    //cy.get('input[type="password"]').type('123456789')
+    //cy.get('input[type="submit"]').click()
+
+
+
+  })   
+  })
+  
